5bc6861f63acc5a625e7764e2fc70ef578fc10b2
Unthink-pri18 <157363325+Unthink-pri18@users.noreply.github.com>
2024-09-27 14:20:44 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,14 +3,21 @@ set -e
 
 echo "Application Name: $APPLICATION_NAME" >> /home/ubuntu/check-env-after.log
 
-if [ "$APPLICATION_NAME" == "testing-variables" ]; then
-    echo "My name is dev" >> /home/ubuntu/check-env-after.log
-elif [ "$APPLICATION_NAME" == "stage" ]; then
-    echo "My name is stage" >> /home/ubuntu/check-env-after.log
-elif [ "$APPLICATION_NAME" == "qa" ]; then
-    echo "My name is qa" >> /home/ubuntu/check-env-after.log
-elif [ "$APPLICATION_NAME" == "preprod" ]; then
-    echo "My name is preprod" >> /home/ubuntu/check-env-after.log
-else
-    echo "Application name is not recognized: $APPLICATION_NAME"
-fi
+case "$APPLICATION_NAME" in
+    "testing-variables")
+        echo "My name is dev" >> /home/ubuntu/check-env-after.log
+        ;;
+    "stage")
+        echo "My name is stage" >> /home/ubuntu/check-env-after.log
+        ;;
+    "qa")
+        echo "My name is qa" >> /home/ubuntu/check-env-after.log
+        ;;
+    "preprod")
+        echo "My name is preprod" >> /home/ubuntu/check-env-after.log
+        ;;
+    *)
+        echo "Application name is not recognized: $APPLICATION_NAME" >> /home/ubuntu/check-env-after.log
+        ;;
+esac
+
