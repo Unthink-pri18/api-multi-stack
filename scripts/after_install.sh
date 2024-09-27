fbfd6f8a922e73648f5ec024357224ff6fa39395
Unthink-pri18 <157363325+Unthink-pri18@users.noreply.github.com>
2024-09-27 14:08:54 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,3 +2,15 @@
 set -e
 
 echo "Application Name: $APPLICATION_NAME" >> /home/ubuntu/check-env-after.log
+
+if [ "$APP_NAME" == "dev" ]; then
+    echo "My name is dev" >> /home/ubuntu/check-env-after.log
+elif [ "$APP_NAME" == "stage" ]; then
+    echo "My name is stage" >> /home/ubuntu/check-env-after.log
+elif [ "$APP_NAME" == "qa" ]; then
+    echo "My name is qa" >> /home/ubuntu/check-env-after.log
+elif [ "$APP_NAME" == "preprod" ]; then
+    echo "My name is preprod" >> /home/ubuntu/check-env-after.log
+else
+    echo "Application name is not recognized: $APP_NAME"
+fi
